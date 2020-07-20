a55a136a3a63888039d4343030a10f159ea3719c
matchashiba <recr38@gmail.com>
2020-07-20 22:42:22 +0000

    Switched port numbers around on .env.example Added .env to .gitignore Added process.env.PORT to app.listen argument Created config file for webpack

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 const express = require('express');
 const app = express();
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Listening on port 4000');
+  console.log('Listening on port ', process.env.PORT);
 });
